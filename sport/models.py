fc75f51ba0820e86e0290518456644b2216c7b52
sergiu19996 Porime <s.porime@yahoo.com>
2024-05-07 19:54:08 +0000

    Add event,news,player,team html folder

--- sport/models.py
+++ sport/models.py
@@ -103,7 +103,10 @@ class Comment(models.Model):
 
     event = models.ForeignKey('Event', on_delete=models.CASCADE)
     content = models.TextField(max_length=200)
-    author = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
